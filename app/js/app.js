1ff770293493036d4c3a743dd4b7aa13db5d368f
lev <levuon@163.com>
2017-04-19 18:15:08 +0800

    format code

--- app/js/app.js
+++ app/js/app.js
@@ -6,17 +6,17 @@ define(["angular", "logicMoon", 'ui.router', "local-storage"], function() {
   var moon = angular.module("moon", ["ui.router", "controllers", "directives", "services", "route", "LocalStorageModule"]);
   
   moon.config(['$stateProvider', '$urlRouterProvider', '$httpProvider', "sessionStorageServiceProvider", "localStorageServiceProvider", "$sceDelegateProvider",
-	function($stateProvider, $urlRouterProvider, $httpProvider, sessionStorageServiceProvider, localStorageServiceProvider, $sceDelegateProvider) {
-	  //初始化 设置sessionStorage prefix为moonSession, localStorage prefix为moonLocal.
-	  
-	  sessionStorageServiceProvider.setPrefix("moonSession");
-	  localStorageServiceProvider.setPrefix('moonLocal');
-	  
-	  // $urlRouterProvider.when('/tabs/selected', "/tabs/fund");
-	  // $urlRouterProvider.when('/asset/recharge', "/recharge");
-	  
-	  $urlRouterProvider.otherwise('/home');
-	}]);
+    function($stateProvider, $urlRouterProvider, $httpProvider, sessionStorageServiceProvider, localStorageServiceProvider, $sceDelegateProvider) {
+      //初始化 设置sessionStorage prefix为moonSession, localStorage prefix为moonLocal.
+      
+      sessionStorageServiceProvider.setPrefix("moonSession");
+      localStorageServiceProvider.setPrefix('moonLocal');
+      
+      // $urlRouterProvider.when('/tabs/selected', "/tabs/fund");
+      // $urlRouterProvider.when('/asset/recharge', "/recharge");
+      
+      $urlRouterProvider.otherwise('/home');
+    }]);
   
   
   return moon;
